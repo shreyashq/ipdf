cd70c0e5819d1f2264c2435bc0d0507122460b1d
shrey <shrey.shreyashh@gmail.com>
2025-01-26 13:09:46 +0530

    added functions and event listeners to handle file upload

--- src/script.js
+++ src/script.js
@@ -40,41 +40,97 @@ btn.addEventListener("click", async () => {
   }
 });
 
+//UNDER CONSTRUCTION FROM HERE
 const submitBtn = document.getElementById("submitBtn");
 const convertBtnEpub = document.getElementById("convertBtnEpub");
-const convertBtnDocx = document.getElementById("convertBtnMobi");
+const convertBtnDocx = document.getElementById("convertBtnDocx");
 const convertBtnMobi = document.getElementById("convertBtnMobi");
 
-submitBtn.addEventListener("click", () => {
-  const value = document.getElementById("fileFormat").value;
-  console.log(value);
+//disable all convert buttons by default
+convertBtnEpub.disabled = true;
+convertBtnDocx.disabled = true;
+convertBtnMobi.disabled = true;
 
-  //Toggling classes based on user selection
-  const epubClass = document.querySelector(".epub");
-  const docxClass = document.querySelector(".docx");
-  const mobiClass = document.querySelector(".mobi");
+//convert button wont be clicked unless file uploaded
+const fileInputEpub = document.getElementById("fileInputEpub");
+const fileInputDocx = document.getElementById("fileInputDocx");
+const fileInputMobi = document.getElementById("fileInputMobi");
 
-  if (value === "epub") {
-    //remove existing classes
-    docxClass.classList.remove("show");
-    mobiClass.classList.remove("show");
-
-    epubClass.classList.toggle("show");
+//enabling Epub Convert button on file upload
+fileInputEpub.addEventListener("change", () => {
+  if (fileInputEpub.files.length > 0) {
+    convertBtnEpub.disabled = false;
+  } else {
+    convertBtnEpub.disabled = true;
   }
+});
 
-  if (value === "docx") {
-    //remove existing classes
-    epubClass.classList.remove("show");
-    mobiClass.classList.remove("show");
-
-    docxClass.classList.toggle("show");
+//enabling Docx Convert button on file upload
+fileInputDocx.addEventListener("change", () => {
+  if (fileInputDocx.files.length > 0) {
+    convertBtnDocx.disabled = false;
+  } else {
+    convertBtnDocx.disabled = "true";
   }
+});
 
-  if (value === "mobi") {
-    //remove existing classes
-    docxClass.classList.remove("show");
-    epubClass.classList.remove("show");
-
-    mobiClass.classList.toggle("show");
+//enabling Mobi Convert on file upload
+fileInputMobi.addEventListener("change", () => {
+  if (fileInputMobi.files.length > 0) {
+    convertBtnMobi.disabled = false;
+  } else {
+    convertBtnMobi.disabled = "true";
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Functions for file convertions
+  //Epub to Pdf
+  convertBtnEpub.addEventListener("click", () => {
+    console.log("inside epub");
+  });
+
+  //Mobi to Pdf
+  convertBtnMobi.addEventListener("click", () => {
+    console.log("inside mobi");
+  });
+
+  //Docx to Pdf
+  convertBtnDocx.addEventListener("click", () => {
+    console.log("inside docx");
+  });
+
+  submitBtn.addEventListener("click", () => {
+    const value = document.getElementById("fileFormat").value;
+    console.log(value);
+
+    //Toggling classes based on user selection
+    const epubClass = document.querySelector(".epub");
+    const docxClass = document.querySelector(".docx");
+    const mobiClass = document.querySelector(".mobi");
+
+    if (value === "epub") {
+      //remove existing classes
+      docxClass.classList.remove("show");
+      mobiClass.classList.remove("show");
+
+      epubClass.classList.toggle("show");
+    }
+
+    if (value === "docx") {
+      //remove existing classes
+      epubClass.classList.remove("show");
+      mobiClass.classList.remove("show");
+
+      docxClass.classList.toggle("show");
+    }
+
+    if (value === "mobi") {
+      //remove existing classes
+      docxClass.classList.remove("show");
+      epubClass.classList.remove("show");
+
+      mobiClass.classList.toggle("show");
+    }
+  });
 });
